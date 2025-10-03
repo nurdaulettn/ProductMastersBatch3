973d16234ef828de170aab356a011ecc2638269a
nurdaulettn <nurdaulettolegen26@gmail.com>
2025-10-03 16:47:19 +0500

    I have done hard task

--- module_4/src/Homework/hard/Main.java
+++ module_4/src/Homework/hard/Main.java
@@ -5,6 +5,8 @@ public class Main {
   public static void main(String[] args) {
     Box<String> box1 = new Box<>("Hello");
     System.out.println(box1.getItem());
+    box1.setItem("World");
+    System.out.println(box1.getItem());
     box1.showType();
     Box<Integer> box2 = new Box<>(543890);
     System.out.println(box2.getItem());
