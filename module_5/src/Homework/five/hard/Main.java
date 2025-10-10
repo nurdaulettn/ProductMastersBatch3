c9110f57c128af6fcc53b117f347025081a6c796
nurdaulettn <nurdaulettolegen26@gmail.com>
2025-10-10 17:47:22 +0500

    I have done hard task

--- module_5/src/Homework/five/hard/Main.java
+++ module_5/src/Homework/five/hard/Main.java
@@ -1,14 +1,29 @@
 package Homework.five.hard;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    System.out.print("n: ");
+    int n = sc.nextInt();
+    ArrayList<Integer> nums = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      nums.add(sc.nextInt());
+    }
+    nums = removeDuplicates(nums);
+    System.out.println(nums);
   }
 
   public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
-    return null;
+    for(int i = 0; i < list.size(); i++) {
+      int n = list.get(i);
+      for(int j = i+1; j < list.size(); j++) {
+        if(n == list.get(j)) list.remove(j);
+      }
+    }
+    return list;
   }
 }
